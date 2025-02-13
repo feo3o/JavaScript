b996613df872803747aea06d5b05635b64618831
Felipe Santos <151412382+feo3o@users.noreply.github.com>
2025-02-13 08:40:37 -0300

    Add files via upload

---
+++ temperature_conversion/index.js
@@ -0,0 +1,25 @@
+const text = document.getElementById("text");
+const c = document.getElementById("c");
+const f = document.getElementById("f");
+const placeholder = document.getElementById("placeholder");
+let temperatura;
+
+function conversion()
+{
+    if (!f.checked && !c.checked)
+    {
+        placeholder.textContent = "Selecione um tipo de conversão.";
+    }
+    else if (c.checked)
+    {
+        temperatura = Number(text.value);
+        temperatura = temperatura * 1.8 + 32;
+        placeholder.textContent = temperatura.toFixed(1) + "°F";
+    }
+    else if (f.checked)
+        {
+            temperatura = Number(text.value);
+            temperatura = (temperatura - 32) / 1.8;
+            placeholder.textContent = temperatura.toFixed(1) + "°C";
+        }
+}
